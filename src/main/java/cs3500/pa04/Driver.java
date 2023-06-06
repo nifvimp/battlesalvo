f704f3e127ea9d2cad3ef6fe72d67e35dfb71eca
nifvimp <kylewmoy@gmail.com>
2023-06-06 00:58:08 -0400

    Implemented Basic Server communications.

--- src/main/java/cs3500/pa04/Driver.java
+++ src/main/java/cs3500/pa04/Driver.java
@@ -2,7 +2,9 @@ package cs3500.pa04;
 
 import cs3500.pa04.controller.Controller;
 import cs3500.pa04.controller.LocalController;
-import cs3500.pa04.controller.InputCollector;
+import cs3500.pa04.controller.ProxyController;
+import cs3500.pa04.controller.UserCommunicator;
+import cs3500.pa04.model.ArtificialPlayer;
 import cs3500.pa04.model.BoardObserver;
 import cs3500.pa04.model.Player;
 import cs3500.pa04.view.GameView;
@@ -21,11 +23,21 @@ public class Driver {
    */
   public static void main(String[] args) {
     GameView view = new TerminalView();
-    InputCollector requester = new InputCollector(view);
     BoardObserver observer = new BoardObserver();
-    Player player = new ArtificialPlayer(observer);
-    Player opponent = new ArtificialPlayer(observer);
-    Controller controller = new LocalController(player, opponent, view, requester, observer);
+    UserCommunicator requester = new UserCommunicator(view);
+    Controller controller;
+    if (args.length == 0) {
+      Player player = new ArtificialPlayer(observer);
+      Player opponent = new ArtificialPlayer(observer);
+      controller = new LocalController(player, opponent, view, requester, observer);
+    } else {
+      try {
+        controller = new ProxyController(new Socket(), new ArtificialPlayer(observer));
+      } catch (IOException e) {
+        System.err.println("There was an error connecting to the server.");
+        return;
+      }
+    }
     controller.run();
   }
 }
--- src/main/java/cs3500/pa04/controller/ProxyController.java
+++ src/main/java/cs3500/pa04/controller/ProxyController.java
@@ -33,8 +33,20 @@ public class ProxyController implements Controller {
   private final Socket server;
   private final Player player;
 
-  public ProxyController(Socket socket, Player player) {
-    this.socket = socket;
+
+  // TODO: don't throw
+
+  /**
+   * Construct an proxy controller.
+   *
+   * @param server the socket connection to the server
+   * @param player the instance of the player
+   * @throws IOException if IOException occurs
+   */
+  public ProxyController(Socket server, Player player) throws IOException {
+    this.server = server;
+    this.out = new PrintStream(server.getOutputStream());
+    this.in = server.getInputStream();
     this.player = player;
   }
 
@@ -49,6 +61,121 @@ public class ProxyController implements Controller {
       }
     } catch (IOException e) {
       // Disconnected from server or parsing exception
+    }
+  }
+
+  /**
+   * Determines the type of request the server has sent and delegates to the
+   * corresponding helper method with the message arguments.
+   *
+   * @param message the MessageJSON used to determine what the server has sent
+   */
+  private void delegateMessage(MessageJson message) {
+    String name = message.name();
+    JsonNode arguments = message.arguments();
+    switch (name) {
+      case "join" -> handleJoin();
+      case "setup" -> handleSetup(arguments);
+      case "take-shots" -> handleTakeShots();
+      case "report-damage" -> handleReportDamage(arguments);
+      case "successful-hits" -> handleSuccessfulHits(arguments);
+      case "end-game" -> endGame(arguments);
+      default -> throw new IllegalStateException("Invalid message name");
+    }
+  }
+
+  private void handleJoin() {
+    MessageJson response = new MessageJson(
+        "join", MAPPER.createObjectNode()
+        .set("arguments", MAPPER.createObjectNode()
+            .put("name", "nifvimp")
+            .put("game-type", GameType.SINGLE.name())
+        )
+    );
+    JsonNode jsonResponse = serializeRecord(response);
+    this.out.println(jsonResponse);
+  }
+
+  private void handleSetup(JsonNode arguments) {
+    int width = arguments.get("width").asInt();
+    int height = arguments.get("height").asInt();
+    JsonNode fleetSpec = arguments.get("fleet-spec");
+    Map<ShipType, Integer> specifications = new HashMap<>();
+    for (ShipType shipType : ShipType.values()) {
+      specifications.put(shipType, fleetSpec.get(shipType.name()).asInt(0));
+    }
+    List<Ship> placements = player.setup(height, width, specifications);
+    FleetJson response = new FleetJson(placements.stream().map(Ship::toJson).toList());
+    JsonNode jsonResponse = serializeRecord(
+        new MessageJson("setup",
+            serializeRecord(response))
+    );
+    this.out.println(jsonResponse);
+  }
+
+  private void handleTakeShots() {
+    List<Coord> shots = player.takeShots();
+    VolleyJson response = new VolleyJson(shots.stream().map(Coord::toJson).toList());
+    JsonNode jsonResponse = serializeRecord(
+        new MessageJson("take-shots",
+            serializeRecord(response))
+    );
+    this.out.println(jsonResponse);
+  }
+
+  private void handleReportDamage(JsonNode arguments) {
+    List<Coord> shots = new ArrayList<>();
+    for (JsonNode node : arguments.get("coordinates")) {
+      shots.add(MAPPER.convertValue(node, CoordJson.class).toCoord());
+    }
+    List<Coord> successful = player.reportDamage(shots);
+    VolleyJson response = new VolleyJson(successful.stream().map(Coord::toJson).toList());
+    JsonNode jsonResponse = serializeRecord(
+        new MessageJson("report-damage",
+            serializeRecord(response))
+    );
+    this.out.println(jsonResponse);
+  }
+
+  private void handleSuccessfulHits(JsonNode arguments) {
+    List<Coord> shots = new ArrayList<>();
+    for (JsonNode node : arguments.get("coordinates")) {
+      shots.add(MAPPER.convertValue(node, CoordJson.class).toCoord());
+    }
+    player.successfulHits(shots);
+    JsonNode jsonResponse = serializeRecord(
+        new MessageJson("successful-hits", MAPPER.createObjectNode())
+    );
+    this.out.println(jsonResponse);
+  }
+
+  private void endGame(JsonNode arguments) {
+    GameResult result = GameResult.valueOf(arguments.get("result").textValue());
+    String reason = arguments.get("reason").textValue();
+    player.endGame(result, reason);
+    JsonNode jsonResponse = serializeRecord(
+        new MessageJson("end-game", MAPPER.createObjectNode())
+    );
+    this.out.println(jsonResponse);
+    try {
+      server.close();
+    } catch (IOException ignored) {
+      // An empty catch block
+    }
+  }
+
+  /**
+   * Converts a given record object to a JsonNode.
+   *
+   * @param record the record to convert
+   * @return the JsonNode representation of the given record
+   * @throws IllegalArgumentException if the record could not be converted correctly
+   */
+  private JsonNode serializeRecord(Record record) throws IllegalArgumentException {
+    try {
+      return MAPPER.convertValue(record, JsonNode.class);
+    } catch (IllegalArgumentException e) {
+      throw new IllegalArgumentException("Given record cannot be serialized");
     }
   }
 }
